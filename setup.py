7a5ecf046d55d808d3e5f5f829997fa9ea3f7d7d
Lion Ackermann <ackerm.lion@hotmail.de>
2018-10-18 13:33:56 +0200

    Add setup.py, Restructure

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""
+walnut: A minimalistic trading engine middleware
+"""
+
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='walnut',
+    version='0.0.1',
+    description=__doc__,
+    long_description=long_description,
+    author='Lion Ackermann',
+    url='https://github.com/liona24/walnut',
+    packages=['walnut'],
+    package_dir={ '': 'src' },
+    package_data={ 'walnut': ['schema_definitions.json'] },
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3'
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
